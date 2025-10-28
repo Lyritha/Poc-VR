4c8060ffc02bcdd1553fe95200fb746a9ff5da47
AlkanTCC <145324845+AlkanTCC@users.noreply.github.com>
2025-10-28 11:09:54 +0100

    SkeeBallMachine

---
+++ Poc-VRProject/Assets/Scripts/SkeeBallMachine/SkeeBall.cs
@@ -0,0 +1,38 @@
+using Oculus.Interaction;
+using Oculus.Interaction.HandGrab;
+using UnityEngine;
+
+public class SkeeBall : MonoBehaviour
+{
+    [SerializeField] HandGrabInteractable grabbableString;
+ 
+    private void Awake()
+    {
+        if (grabbableString == null) return;
+ 
+        grabbableString.WhenSelectingInteractorRemoved.Action += OnGrabEnd;
+    }
+ 
+    private void OnDestroy()
+    {
+        if (grabbableString == null) return;
+ 
+        grabbableString.WhenSelectingInteractorRemoved.Action -= OnGrabEnd;
+    }
+ 
+    private void OnGrabEnd(IInteractor interactor)
+    {
+        SkeeBallMachine.Instance.RemoveBall();
+
+        Destroy(this, 10);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<Points>(out Points points))
+        {
+            SkeeBallMachine.Instance.AddTotalPoints(points.GetPoints());
+            print(points.GetPoints());
+        }
+    }
+}
---
+++ Poc-VRProject/Assets/Scripts/SkeeBallMachine/SkeeBallMachine.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class SkeeBallMachine : UnitySingleton<SkeeBallMachine>
+{
+    [SerializeField] UpdatePointText updatePointText;
+    [SerializeField] GameObject ballObject;
+    [SerializeField] Transform ballSpawnPoint;
+
+    int totalPoints = 0;
+
+    int ballCount = 0;
+    int maxBallCount = 6;
+
+    void Start()
+    {
+        SpawnAllBalls();
+    }
+
+    public void AddTotalPoints(int pPoints)
+    {
+        totalPoints += pPoints;
+        updatePointText.UpdateText(totalPoints);
+    }
+
+    public void ResetTotalPoints()
+    {
+        totalPoints = 0;
+        updatePointText.UpdateText(totalPoints);
+    }
+
+    public void RemoveBall()
+    {
+        ballCount--;
+        if (ballCount <= 0)
+        {
+            GiveTickets();
+        }
+    }
+    
+    public void ResetBallCount() => ballCount = maxBallCount;
+
+    void SpawnAllBalls()
+    {
+        ResetTotalPoints();
+        for (int i = 0; i < maxBallCount; i++)
+        {
+            Invoke("SpawnBall", 0.5f * i);
+        }
+    }
+
+    void SpawnBall()
+    {
+        GameObject newBallObject = Instantiate(ballObject);
+        newBallObject.transform.position = ballSpawnPoint.position;
+    }
+    
+    void GiveTickets()
+    {
+        TicketManager.Instance.AddTicket(totalPoints);
+    }
+}
