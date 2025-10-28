256f7b565d74531048f9c815ecdd1993f9ede761
roni702 <152854987+roni702@users.noreply.github.com>
2025-10-28 13:49:08 +0100

    Test

--- Poc-VRProject/Assets/Scripts/SkeeBallMachine/SkeeBall.cs
+++ Poc-VRProject/Assets/Scripts/SkeeBallMachine/SkeeBall.cs
@@ -6,33 +6,28 @@ public class SkeeBall : MonoBehaviour
 {
     [SerializeField] HandGrabInteractable grabbableString;
     [SerializeField] Rigidbody rb;
- 
+
     private void Awake()
     {
         if (grabbableString == null) return;
- 
         grabbableString.WhenSelectingInteractorRemoved.Action += OnGrabEnd;
     }
- 
+
     private void OnDestroy()
     {
         if (grabbableString == null) return;
- 
         grabbableString.WhenSelectingInteractorRemoved.Action -= OnGrabEnd;
     }
- 
+
     void OnGrabEnd(IInteractor interactor)
     {
+        // Verdubbel de snelheid direct nadat de speler de bal loslaat
+        rb.linearVelocity *= 20f;
+
+        // Daarna doe je je andere acties
         SkeeBallMachine.Instance.RemoveBall();
-        Rigidbody handRb = ((MonoBehaviour)interactor).GetComponent<Rigidbody>();
-
-        float velocityMultiplier = 2f;
-        rb.linearVelocity = handRb.linearVelocity * velocityMultiplier;
-        rb.angularVelocity = handRb.angularVelocity * velocityMultiplier;
-
         Destroy(gameObject, 10);
     }
-
 
     void OnTriggerEnter(Collider other)
     {
