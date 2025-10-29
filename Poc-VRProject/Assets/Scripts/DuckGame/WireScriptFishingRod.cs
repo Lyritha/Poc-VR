8d10a3a21bfdcfc5794f62eb82534df674b06c57
THERMELINK <thermelink01@student.rocvantwente.nl>
2025-10-29 14:48:55 +0100

    feat: fixed fishing rod, added script for floater
    fixed no behavior on the fishing rod
    added a script for the wire from rod to floater
    added a script for floater behavior (keeping it in a certain range)

--- Poc-VRProject/Assets/Scripts/DuckGame/WireScriptFishingRod.cs
+++ Poc-VRProject/Assets/Scripts/DuckGame/WireScriptFishingRod.cs
@@ -4,19 +4,46 @@ public class WireScriptFishingRod : MonoBehaviour
 {
     GameObject wirePointFishingRod;
     [SerializeField] GameObject wirePointEndPoint;
-    float wireSegmentCount = 10;
+    int wireSegmentCount = 3;
     float wireDanglingEffectStrongness = 0.3f;
     LineRenderer fishingLine;
+
+    public Vector3 posA;
+    public Vector3 posB;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         wirePointFishingRod = gameObject;
         fishingLine = gameObject.GetComponent<LineRenderer>();
+        fishingLine.positionCount = wireSegmentCount;
+        fishingLine.SetWidth(0.05f, 0.05f);
+        fishingLine.SetColors(Color.white, Color.white);
     }
 
     // Update is called once per frame
     void Update()
     {
+        posA = wirePointFishingRod.transform.position;
+        posB = wirePointEndPoint.transform.position;
 
+        for (int i = 0; i < wireSegmentCount; i++)
+        {
+            Vector3 currentPoint = Vector3.Lerp(posA, posB, (i / (float)wireSegmentCount));
+            if(i == wireSegmentCount-1) currentPoint = posB;
+            fishingLine.SetPosition(i,currentPoint);
+        }
+        KeepFloaterInDistance(1.5f, posA,wirePointEndPoint);
+    }
+
+
+    void KeepFloaterInDistance(float radius, Vector3 posA, GameObject floater)
+    {
+        float distance = Vector3.Distance(posA, floater.transform.position);
+        if(distance >= radius)
+        {
+            Vector3 richting = (floater.transform.position- posA).normalized;
+            floater.transform.position = posA + richting * radius;
+        }
     }
 }
